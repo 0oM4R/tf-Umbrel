d82a204b6ac50c4321e58c11591a87b878bdc33e
kassem <omarksm09@gmail.com>
2023-02-15 11:19:11 +0200

    resolve registration proccess error

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -eox pipefai
+set -eox pipefail
 while curl http://10.21.21.4:3006 ; [ $? -eq 7 ];do
     echo manager not ready;
    sleep 20; 
   done
-curl -v -X POST  http://10.21.21.4:3006/v1/account/register -H "Content-Type: application/json" -d '{"name": "${USERNAME}", "password": "${PASSWORD}"}'
+curl -v -X POST  http://10.21.21.4:3006/v1/account/register -H "Content-Type: application/json" -d '{"name": "'"${USERNAME}"'", "password": "'"${PASSWORD}"'"}'
